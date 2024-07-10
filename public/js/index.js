02521af786fb9be0e23d450bbf8f73f09e492cb0
Delta Student <akashdeypersonal17@gmail.com>
2024-07-11 04:44:21 +0530

    FullStack Project of Durvah is completed

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector('.header');
+
+mobile_nav.addEventListener('click', () => {
+     nav_header.classList.toggle("active");
+});
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
+
